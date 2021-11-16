37576a6eb27b785d7a5d0c08784a1d9aa5165737
“Ben <BRyding@am.trimblecorp.net>
2021-11-16 13:55:02 -0700

    removed dead code from app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 1956
 
 const path = require('path');
 
-// app.set('appPath', 'output');
 app.use(express.static(__dirname));
 
 express.static.mime.define({ 'application/wasm': ['wasm'] });
@@ -13,7 +12,7 @@ app.get('*', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(1956, function (req, res) {
+var server = app.listen(port, function (req, res) {
   var host = server.address().address
   var port = server.address().port
   console.log("app listening at", host, port)
